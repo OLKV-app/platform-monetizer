77dc453a3bcf463ebad2c283387a7aa1aef8cb7b
OLKV-app <olkv7229@gmail.com>
2026-07-23 13:59:33 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,15 +16,14 @@ import {
 } from "firebase/auth";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBjryYFJ2owb7C0PQX3_zJ8QoXV6e1Eqk4",
-  authDomain: "olkv-a8199.firebaseapp.com",
-  projectId: "olkv-a8199",
-  storageBucket: "olkv-a8199.firebasestorage.app",
-  messagingSenderId: "97575529926",
-  appId: "1:97575529926:web:28f398e0f3c8921faf16c7",
-  measurementId: "G-9YH9SXMK31",
+  apiKey: "AIzaSyCRFHtpCaaYg1cQXIIFuOE6rWgokZp5_Ho",
+  authDomain: "project-6e03e9ed-73b5-4bf4-816.firebaseapp.com",
+  projectId: "project-6e03e9ed-73b5-4bf4-816",
+  storageBucket: "project-6e03e9ed-73b5-4bf4-816.firebasestorage.app",
+  messagingSenderId: "574188400487",
+  appId: "1:574188400487:web:11a0b6f64c775adc0f09c2",
+  measurementId: "G-9ZDH0TRWNG"
 };
-
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
